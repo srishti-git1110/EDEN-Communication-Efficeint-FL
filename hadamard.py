ac4a34a0cdf35c865540fa3f6ecbe2da1ffe4643
Srishti Gureja <84127491+srishti-git1110@users.noreply.github.com>
2023-09-25 01:21:41 +0530

    Update hadamard.py

--- hadamard.py
+++ hadamard.py
@@ -1,1 +1,14 @@
+import torch
+import torch.nn.functional as F
+from math import floor, log2
 
+
+# refer section 5 of the paper (pg. 8)
+def hadamard_walsh_transform_(grads):
+  '''This function is used to apply an inplace randomized hadamard transform to the gradient vector prior to its quantization.
+
+  :grads: a vector with even no. of elements containing the gradients wrt all model parameters
+  :return: Hadamard transformed gradient vector
+  '''
+
+ 
